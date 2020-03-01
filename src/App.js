b8c830674d10a97d093546ae964a394ceb9dd856
Valdir Coxev <valdircoxevdev@gmail.com>
2020-03-01 19:59:10 -0400

    Adding component at  app.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import React from "react";
 
 import GlobalStyle from "./styles/Global";
 import Header from "./components/Header";
+import Balance from "./components/Balance";
+import IncomeExpense from "./components/IncomeExpenses";
+import TransactionList from "./components/TransactionList";
 
 function App() {
   return (
     <>
       <GlobalStyle />
       <Header />
+      <Balance />
+      <IncomeExpense />
+      <TransactionList />
     </>
   );
 }
